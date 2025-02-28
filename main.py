aa9d1cfbef161472967c89e877b0e897ffed59be
kub5on <jaakub.pankowski@gmail.com>
2025-02-28 22:39:38 +0100

    all pages

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 
-with open ("output/index.html", "r") as f:
-    doc = BeautifulSoup(f, "html.parser")
 
-print(doc.prettify())
+def parse_local_html(file_path):
+    with open(file_path, "r") as file:
+        soup = BeautifulSoup(file, 'lxml')
+        return soup
 
+
+path = "output/index.html"
+soup = parse_local_html(path)
+
+boxes = soup.select("div.year-card")
+
+years = []
+links = []
+
+for box in boxes:
+    years.append(box.find('h2').text)
+    links.append(box.find('a')['href'])
+
+df = pd.DataFrame({'years': years, 'links': links})
+print(df)
+
+df.to_csv('linki.csv', index=False)
+
+# lista = []
+#
+# for link in links:
+#     soup = parse_local_html(link)
+#     nominowani = soup.select('.nominee')
+#     for nominee in nominowani:
+#         lista.append({
+#             "link": link,
+#             "name": nominee.find('p').text
+#         })
+#
+# df = pd.DataFrame(lista).drop_duplicates()
+# df.to_csv('nominowani.csv', index=False)
